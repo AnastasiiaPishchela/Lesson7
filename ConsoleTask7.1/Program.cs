2e9730ddd5fa55c0cfbaad5b8d90bd726ff4c741
AnastasiiaPishchela <51374113+AnastasiiaPishchela@users.noreply.github.com>
2019-08-07 16:10:17 +0300

    Add files via upload

---
+++ ConsoleTask7.1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTask7._1
+{
+    //    Есть отношения наследования: фигура(Shape) – базовый класс, треугольник (Triangle), круг (Circle),
+    //прямоугольник (Rectangle) – производные классы от Shape, равнобедренный треугольник
+    //(IsoscelesTriangle) – от треугольника, квадрат (Square) – от прямоугольника.Shape имеет методы
+    //GetАrea() – возвращает площадь фигуры и GetName() – возвращает название фигуры1
+    //.В каждом из
+    //производных классов можно определить площадь и узнать имя фигуры. Разработать консольное
+    //приложение для демонстрации принципа полиморфизма, используя вывод текстовых сообщений о
+    //названии и площади фигуры.
+    //Выполнить задание при условии, что фигура (Shape) – интерфейс
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(3, 5, 7);
+            Circle circle = new Circle(15);
+            Rectangle rectangle = new Rectangle(7, 13);
+            IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(5, 8);
+            Square square = new Square(4);
+            Console.WriteLine($"Shape name: {triangle.GetName()}  Shape square: {triangle.GetArea()}");
+            Console.WriteLine($"Shape name: {circle.GetName()}  Shape square: {circle.GetArea()}");
+            Console.WriteLine($"Shape name: {rectangle.GetName()}  Shape square: {rectangle.GetArea()}");
+            Console.WriteLine($"Shape name: {isoscelesTriangle.GetName()}  Shape square: {isoscelesTriangle.GetArea()}");
+            Console.WriteLine($"Shape name: {square.GetName()}  Shape square: {square.GetArea()}");
+            Console.ReadKey();
+        }
+    }
+
+    public interface Shape
+    {
+
+        double GetArea();
+        string GetName();
+
+    }
+
+    public class Triangle : Shape
+    {
+        int a;
+        int b;
+        int c;
+
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double GetArea()
+        {
+            double p = (a + b + c) / 2.0;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public virtual string GetName()
+        {
+            return "Triangle";
+        }
+
+    }
+
+    public class Circle : Shape
+    {
+        int radious;
+
+        public Circle(int radious)
+        {
+            this.radious = radious;
+        }
+        public double GetArea()
+        {
+            return 3.14 * radious * radious;
+        }
+
+        public string GetName()
+        {
+            return "Circle";
+        }
+
+    }
+
+    public class Rectangle : Shape
+    {
+        int oneSide;
+        int twoSide;
+
+        public Rectangle(int oneSide, int twoSide)
+        {
+            this.oneSide = oneSide;
+            this.twoSide = twoSide;
+        }
+        public double GetArea()
+        {
+            return oneSide * twoSide;
+        }
+
+        public virtual string GetName()
+        {
+            return "Rectangle";
+        }
+
+    }
+
+    public class IsoscelesTriangle : Triangle
+    {
+
+        public IsoscelesTriangle(int baseSide, int equalSide) : base(baseSide, equalSide, equalSide)
+        {
+
+        }
+
+        public override string GetName()
+        {
+            return "IsoscelesTriangle";
+        }
+
+    }
+
+    public class Square : Rectangle
+    {
+
+        public Square(int side) : base(side, side)
+        {
+
+        }
+
+        public string GetName()
+        {
+            return "Square";
+        }
+    }
+
+
+}
---
+++ ConsoleTask7.2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTask7._2
+{
+    //Создать класс «животные», производные классы «хищники» и «травоядные», затем производные классы
+    //«волк», «лиса», «заяц», «олень». Содержимое класса – на собственное усмотрение.Создать массив
+    //объектов с произвольным количеством экземпляров всех указанных классов и подсчитать, сколько в
+    //массиве хищников и травоядных, волков, лис, зайцев и оленей2
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal> { new Predator(), new Herbivores(), new Herbivores(), new Wolf(), new Wolf(), new Wolf(), new Fox(), new Fox(), new Fox(), new Fox(),
+                new Rabbit(), new Rabbit(), new Rabbit(), new Rabbit(), new Rabbit(), new Deer(), new Deer(), new Deer(), new Deer(), new Deer(), new Deer()};
+            IDictionary<string, int> table = new Dictionary<string, int>()
+            {
+                 { "Fox", 0},
+                 { "Wolf", 0},
+                 { "Rabbit", 0},
+                 { "Deer", 0},
+                 { "Predator", 0},
+                 { "Herbivores", 0}
+            };
+            foreach (Animal animal in animalList)
+            {
+                switch (animal)
+                {
+                    case Fox p:
+                        table["Fox"] = table["Fox"] + 1;
+                        break;
+                    case Wolf p:
+                        table["Wolf"] = table["Wolf"] + 1;
+                        break;
+                    case Rabbit p:
+                        table["Rabbit"] = table["Rabbit"] + 1;
+                        break;
+                    case Deer p:
+                        table["Deer"] = table["Deer"] + 1;
+                        break;
+                    case Predator p:
+                        table["Predator"] = table["Predator"] + 1;
+                        break;
+                    case Herbivores p:
+                        table["Herbivores"] = table["Herbivores"] + 1;
+                        break;
+                }
+
+            }
+            foreach (KeyValuePair<string, int> entry in table)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}",
+                                                            entry.Key,
+                                                            entry.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string getAnimalName();
+    }
+
+    class Predator : Animal
+    {
+        public override string getAnimalName()
+        {
+            return "Predator";
+        }
+    }
+
+    class Herbivores : Animal
+    {
+        public override string getAnimalName()
+        {
+            return "Herbivores";
+        }
+
+    }
+
+    class Wolf : Predator
+    {
+        public override string getAnimalName()
+        {
+            return "Wolf";
+        }
+
+    }
+
+    class Fox : Predator
+    {
+        public override string getAnimalName()
+        {
+            return "Fox";
+        }
+
+    }
+
+    class Rabbit : Herbivores
+    {
+        public override string getAnimalName()
+        {
+            return "Rabbit";
+        }
+
+    }
+
+    class Deer : Herbivores
+    {
+        public override string getAnimalName()
+        {
+            return "Deer";
+        }
+
+    }
+
+
+}
